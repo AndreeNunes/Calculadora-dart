acbb31c734f8d6753ccd04f54c4ad75541c0973d
André Nicola Nunes <0040481911045@americana.fatec.br>
2020-03-10 12:21:08 -0300

    finalização da lógica da calculadora

--- lib/memory.dart
+++ lib/memory.dart
@@ -1,13 +1,81 @@
 class Memory {
+  static const String OPERATIONS = "+-%x=";
   String result = '0';
+  final _buffer = [0.0, 0.0];
+  int _bufferIndex = 0;
+  bool _operationUsed = false;
+  String _operation;
 
   void applyCommand(String label) {
-    if (label == 'AC')
+    if (label == 'AC') {
       _clear();
-    else
-      result += label;
+    } else if (label == 'DEL') {
+      _deleteEndDigit();
+    } else if (OPERATIONS.contains(label)) {
+      _setOperation(label);
+    } else {
+      _addDigit(label);
+    }
   }
+
+  _deleteEndDigit() {
+    result = result.length == 1 ? '0' : result.substring(0, result.length - 1);
+  }
+
+  _setOperation(String operation) {
+    if (_bufferIndex == 0) {
+      if (operation != '=') {
+        _bufferIndex++;
+      }
+    } else {
+
+      if(operation == '=' || !_operationUsed){
+        _buffer[0] = _calculate();
+        result = _buffer[0].toString();
+        result = result.endsWith('.0') ? result.replaceAll('.0', '') : result;
+      }
+    }
+    _operationUsed = true;
+    if (operation != '=') {
+      _operation = operation;
+    }
+  }
+
+  _calculate() {
+    switch (_operation) {
+      case '+':
+        return _buffer[0] + _buffer[1];
+      case '-':
+        return _buffer[0] - _buffer[1];
+      case 'x':
+        return _buffer[0] * _buffer[1];
+      case '/':
+        return _buffer[0] / _buffer[1];
+      case '%':
+        return _buffer[0] % _buffer[1];
+    }
+  }
+
+  _addDigit(String digit) {
+    if (_operationUsed) result = '0';
+    if (result == '0' && digit != '.') {
+      result = '';
+    }
+
+    if (digit == '.' && result.contains('.')) {
+      digit = '';
+    }
+
+    result += digit;
+    _buffer[_bufferIndex] = double.parse(result);
+    _operationUsed = false;
+  }
+
   _clear() {
     result = '0';
+    _operationUsed = false;
+    _buffer.setAll(0, [0.0, 0.0]);
+    _bufferIndex = 0;
+    _operation = null;
   }
 }
